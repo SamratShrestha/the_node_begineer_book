6e6ebf58d5ccf8a8886e1a5ff40bb4c2b904508e
SamratShrestha1 <np03a180012@heraldcollege.edu.np>
2021-01-15 18:35:21 +0545

    Blocking and Non-Blocking operation

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require('url');
+
+function start(route,handle){
+http.createServer(function(request,response){
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for "+pathname+" received");
+    route(handle,pathname,response);
+    // response.writeHead(200,{"Content-Type":"text/plain"});
+    // response.write("Hello World");
+    // response.end()
+}).listen(8888);
+console.log("Server has started");
+}
+exports.start = start;
+
